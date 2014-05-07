7a0fad7e2b046ff517c15b0af10225f7ecdd4d20
tbicool <tbicool@qq.com>
2014-05-07 15:05:20 +0800

    修改maven

--- src/main/java/com/st/qunar/order/Main.java
+++ src/main/java/com/st/qunar/order/Main.java
@@ -1,11 +1,11 @@
 package com.st.qunar.order;
 
-import org.eclipse.jetty.server.Server;
-import org.springside.modules.test.jetty.JettyFactory;
-import org.springside.modules.test.spring.Profiles;
+import java.io.File;
+import java.net.URL;
+import java.security.ProtectionDomain;
 
-import com.st.qunar.order.service.OrderExportService;
-import com.st.qunar.order.web.util.SpringContextUtil;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 public class Main {
 
@@ -13,31 +13,69 @@ public class Main {
 	public static final String CONTEXT = "/qunar";
 
 	public static void main(String[] args) throws Exception {
-		// 设定Spring的profile1
-		Profiles.setProfileAsSystemProperty(Profiles.PRODUCTION);
+		/*
+		 * // 设定Spring的profile1
+		 * Profiles.setProfileAsSystemProperty(Profiles.PRODUCTION);
+		 * 
+		 * // 启动Jetty
+		 * Server server = JettyFactory.createServerInSource(PORT, CONTEXT);
+		 * // JettyFactory.setTldJarNames(server, TLD_JAR_NAMES);
+		 * 
+		 * try {
+		 * server.start();
+		 * System.out.println("[INFO] Server running at http://localhost:" + PORT + CONTEXT);
+		 * System.out.println("[HINT] Hit Enter to reload the application quickly");
+		 * 
+		 * // Thread.sleep(10 * 1000);
+		 * // 5分钟请求导入一次
+		 * // ((OrderExportService) SpringContextUtil.getBean("orderExportService")).run();
+		 * } catch (Exception e) {
+		 * e.printStackTrace();
+		 * System.exit(-1);
+		 * }
+		 * 
+		 * while (true) {
+		 * char c = (char) System.in.read();
+		 * if (c == '\n') {
+		 * JettyFactory.reloadContext(server);
+		 * }
+		 * }
+		 */
+		String contextPath = "/order";
+		int port = Integer.getInteger("port", 8080);
 
-		// 启动Jetty
-		Server server = JettyFactory.createServerInSource(PORT, CONTEXT);
-		// JettyFactory.setTldJarNames(server, TLD_JAR_NAMES);
+		Server server = createServer(contextPath, port);
 
 		try {
 			server.start();
-			System.out.println("[INFO] Server running at http://localhost:" + PORT + CONTEXT);
-			System.out.println("[HINT] Hit Enter to reload the application quickly");
-
-			Thread.sleep(10 * 1000);
-			// 5分钟请求导入一次
-			((OrderExportService) SpringContextUtil.getBean("orderExportService")).run();
+			server.join();
 		} catch (Exception e) {
 			e.printStackTrace();
-			System.exit(-1);
+			System.exit(100);
 		}
+	}
 
-		while (true) {
-			char c = (char) System.in.read();
-			if (c == '\n') {
-				JettyFactory.reloadContext(server);
-			}
-		}
+	private static Server createServer(String contextPath, int port) {
+		// use Eclipse JDT compiler
+		System.setProperty("org.apache.jasper.compiler.disablejsr199", "true");
+
+		Server server = new Server(port);
+		server.setStopAtShutdown(true);
+
+		ProtectionDomain protectionDomain = Main.class.getProtectionDomain();
+		URL location = protectionDomain.getCodeSource().getLocation();
+		String warFile = location.toExternalForm();
+
+		WebAppContext context = new WebAppContext(warFile, contextPath);
+		context.setServer(server);
+
+		// 设置work dir,war包将解压到该目录，jsp编译后的文件也将放入其中。
+		String currentDir = new File(location.getPath()).getParent();
+		System.out.println(currentDir);
+		File workDir = new File(currentDir, "work");
+		context.setTempDirectory(workDir);
+
+		server.setHandler(context);
+		return server;
 	}
 }
