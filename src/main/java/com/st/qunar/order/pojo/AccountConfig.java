61a975ab4077b084b8915a553d74d4577796fa77
kxhu <kaixuehu@gmail.com>
2014-05-15 21:28:00 +0800

    sec

--- src/main/java/com/st/qunar/order/pojo/AccountConfig.java
+++ src/main/java/com/st/qunar/order/pojo/AccountConfig.java
@@ -8,14 +8,5 @@ package com.st.qunar.order.pojo;
  * 
  */
 public class AccountConfig {
-	public static final String QUNAR_ORDER_EXPORT_URL = "http://ygz.trade.qunar.com/tts/interface/new/orderexport";
-	// public static final String QUNAR_ORDER_EXPORT_URL = "http://localhost:8080/qunar/order/export";
 
-	public static final String QUNAR_ORDER_EXPORT_USER_PARA = "user";
-	public static final String QUNAR_ORDER_EXPORT_USER_VALUE = "ygQunarTtsOrder";
-
-	public static final String QUNAR_ORDER_EXPORT_PASS_PARA = "pass";
-	public static final String QUNAR_ORDER_EXPORT_PASS_VALUE = "K39dKd@8";
-
-	public static final String QUNAR_ORDER_STATUS_UPDATE_SEC_CODE = "D3i#2d@0";
 }
